eee2c34b296ea607423e0755f8f55ddda9927a4f
Freaks <99678890+Hugo-Galley@users.noreply.github.com>
2023-11-16 21:47:46 +0100

    add carouselle

--- fichier_php/index.php
+++ fichier_php/index.php
@@ -1,3 +1,8 @@
+<?php
+include('bdd.php');
+$velo = $bdd->query("SELECT * FROM velos")->fetchAll();
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,7 +11,14 @@
     <link rel="stylesheet" type="text/css" href="../style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">  <title>Acceuil</title>
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">  
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript" src="../script.js"></script>
+
+    <title>Acceuil</title>
 </head>
 <body>
 <header>
@@ -15,11 +27,18 @@
         ?>
 </header>
     <main>
+
         <h1 id="marque">SmartBike</h1>
         <div class="presentation">
-            <img src="../items/smartbike1.png" alt="image velo 1" id="velo1">
-            <p id="pres-text">Bienvenue sur le site de Smartbike, l'entreprise innovante dédiée à la création de solutions de mobilité électrique. Nous sommes fiers de vous présenter notre dernier bijou technologique, le vélo électrique révolutionnaire "Bike5". Chez Smartbike, nous nous engageons à repousser les limites de l'innovation en proposant des alternatives de transport écologiques, durables et élégantes. Explorez notre gamme diversifiée de vélos électriques, comprenant le modèle emblématique "BikeOne", le tout-terrain performant "Bike22" et le modèle phare "BikeFirst". Notre site est conçu pour vous offrir une expérience immersive, où vous pourrez découvrir les caractéristiques uniques de chaque vélo, en apprendre davantage sur notre entreprise et même trouver des conseils d'experts pour choisir le modèle qui correspond le mieux à vos besoins. Rejoignez la révolution de la mobilité électrique avec Smartbike et embarquez pour un avenir plus vert et plus dynamique.</p>
-        </div>
+        <?php
+          foreach ($velo as $imageVelo) {
+          echo '<img src="' . $imageVelo['photo_url'] . '" alt="image velo 1">';
+        }
+        ?>
+  </div>
+  <p id="pres-text">Bienvenue sur le site de Smartbike, l'entreprise innovante dédiée à la création de solutions de mobilité électrique. Nous sommes fiers de vous présenter notre dernier bijou technologique, le vélo électrique révolutionnaire "Bike5". Chez Smartbike, nous nous engageons à repousser les limites de l'innovation en proposant des alternatives de transport écologiques, durables et élégantes. Explorez notre gamme diversifiée de vélos électriques, comprenant le modèle emblématique "BikeOne", le tout-terrain performant "Bike22" et le modèle phare "BikeFirst". Notre site est conçu pour vous offrir une expérience immersive, où vous pourrez découvrir les caractéristiques uniques de chaque vélo, en apprendre davantage sur notre entreprise et même trouver des conseils d'experts pour choisir le modèle qui correspond le mieux à vos besoins. Rejoignez la révolution de la mobilité électrique avec Smartbike et embarquez pour un avenir plus vert et plus dynamique.</p>
+
+
     </main>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,11 @@
+
+  $(document).ready(function(){
+    $('.presentation').slick({
+      autoplay: true,
+      dots: true,
+      infinite: true,
+      speed: 100,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
